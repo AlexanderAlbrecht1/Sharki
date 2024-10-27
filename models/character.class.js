8cea1778567de120c920fbcc41d69eac8fbd7b4c
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-10-27 16:07:54 +0100

    first movement per keypress

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,6 @@
 class Character extends movableObject {
+    speed = 1;
+
     ImagesWatiting = [
         './img/1.Sharkie/1.IDLE/1.png',
         './img/1.Sharkie/1.IDLE/2.png',
@@ -19,16 +21,38 @@ class Character extends movableObject {
         './img/1.Sharkie/1.IDLE/17.png',
         './img/1.Sharkie/1.IDLE/18.png',
     ]
+
+    ImagesSwimming = [
+        'img/1.Sharkie/3.Swim/1.png',
+        'img/1.Sharkie/3.Swim/2.png',
+        'img/1.Sharkie/3.Swim/3.png',
+        'img/1.Sharkie/3.Swim/4.png',
+        'img/1.Sharkie/3.Swim/5.png',
+        'img/1.Sharkie/3.Swim/6.png',
+    ]
     currentImage = 0;
+    world;
 
     constructor() {
         super().loadImage('img/1.Sharkie/1.IDLE/1.png');
         this.loadImages(this.ImagesWatiting);
+        this.loadImages(this.ImagesSwimming);
 
         this.animate();
+        this.moveRight();
     }
 
     animate() {
+
+        // setTimeout(() => {
+        //     setInterval(() => {
+        //         let i = this.currentImage % this.ImagesWatiting.length;
+        //         let path = this.ImagesWatiting[i];
+        //         this.img = this.imageCache[path];
+        //         this.currentImage++
+        //     }, 200)
+
+        // }, 1000);
 
         setInterval(() => {
             let i = this.currentImage % this.ImagesWatiting.length;
@@ -40,8 +64,23 @@ class Character extends movableObject {
     }
 
     moveRight() {
-        console.log('moving right');
-    };
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT) {
+                let i = this.currentImage % this.ImagesSwimming.length;
+                let path = this.ImagesSwimming[i];
+                this.img = this.imageCache[path];
+                this.currentImage++
+            }
+        }, 200)
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+        }, 1000 / 60)
+    }
+
+
 
     moveUp() {
 
