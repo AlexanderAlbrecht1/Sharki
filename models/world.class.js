92f4c2a7a9f2c203d390c44a8a096c2bac0d2a93
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-10-29 23:54:50 +0100

    added level class

--- models/world.class.js
+++ models/world.class.js
@@ -1,41 +1,8 @@
 class World {
     character = new Character();
-    enemies = [
-        new pufferFish(),
-        new pufferFish(),
-        new JellyFish(),
-        new pufferFish(),
-        new pufferFish(),
-        new JellyFish(),
-    ];
+    enemies = level1.enemies;
 
-    backgroundObjects = [ 
-        new BackgroundObjects('img/3. Background/Layers/5. Water/D2.png', -720),
-        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D2.png', -720),
-        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D2.png', -720),
-        new BackgroundObjects('img/3. Background/Layers/2. Floor/D2.png', -720),
-        new BackgroundObjects('img/3. Background/Layers/1. Light/1.png', -720),
-        new BackgroundObjects('img/3. Background/Layers/5. Water/D1.png', 0),
-        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D1.png', 0),
-        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D1.png', 0),
-        new BackgroundObjects('img/3. Background/Layers/2. Floor/D1.png', 0),
-        new BackgroundObjects('img/3. Background/Layers/1. Light/1.png', 0),
-        new BackgroundObjects('img/3. Background/Layers/5. Water/D2.png', 720),
-        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D2.png', 720),
-        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D2.png', 720),
-        new BackgroundObjects('img/3. Background/Layers/2. Floor/D2.png', 720),
-        new BackgroundObjects('img/3. Background/Layers/1. Light/2.png', 720),
-        new BackgroundObjects('img/3. Background/Layers/5. Water/D1.png', 1440),
-        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D1.png', 1440),
-        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D1.png', 1440),
-        new BackgroundObjects('img/3. Background/Layers/2. Floor/D1.png', 1440),
-        new BackgroundObjects('img/3. Background/Layers/1. Light/2.png', 1440),
-        new BackgroundObjects('img/3. Background/Layers/5. Water/D2.png', 2160),
-        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D2.png', 2160),
-        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D2.png', 2160),
-        new BackgroundObjects('img/3. Background/Layers/2. Floor/D2.png', 2160),
-        new BackgroundObjects('img/3. Background/Layers/1. Light/2.png', 2160),
-    ];
+    backgroundObjects = level1.backgroundObjects ;
 
     canvas;
     ctx;
