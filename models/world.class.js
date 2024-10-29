8b85e78bc396d50241ccf7fdc87b1d89b5c27e93
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-10-29 23:37:14 +0100

    larger map

--- models/world.class.js
+++ models/world.class.js
@@ -10,11 +10,31 @@ class World {
     ];
 
     backgroundObjects = [ 
+        new BackgroundObjects('img/3. Background/Layers/5. Water/D2.png', -720),
+        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D2.png', -720),
+        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D2.png', -720),
+        new BackgroundObjects('img/3. Background/Layers/2. Floor/D2.png', -720),
+        new BackgroundObjects('img/3. Background/Layers/1. Light/1.png', -720),
         new BackgroundObjects('img/3. Background/Layers/5. Water/D1.png', 0),
         new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D1.png', 0),
         new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D1.png', 0),
         new BackgroundObjects('img/3. Background/Layers/2. Floor/D1.png', 0),
         new BackgroundObjects('img/3. Background/Layers/1. Light/1.png', 0),
+        new BackgroundObjects('img/3. Background/Layers/5. Water/D2.png', 720),
+        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D2.png', 720),
+        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D2.png', 720),
+        new BackgroundObjects('img/3. Background/Layers/2. Floor/D2.png', 720),
+        new BackgroundObjects('img/3. Background/Layers/1. Light/2.png', 720),
+        new BackgroundObjects('img/3. Background/Layers/5. Water/D1.png', 1440),
+        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D1.png', 1440),
+        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D1.png', 1440),
+        new BackgroundObjects('img/3. Background/Layers/2. Floor/D1.png', 1440),
+        new BackgroundObjects('img/3. Background/Layers/1. Light/2.png', 1440),
+        new BackgroundObjects('img/3. Background/Layers/5. Water/D2.png', 2160),
+        new BackgroundObjects('img/3. Background/Layers/3.Fondo 1/D2.png', 2160),
+        new BackgroundObjects('img/3. Background/Layers/4.Fondo 2/D2.png', 2160),
+        new BackgroundObjects('img/3. Background/Layers/2. Floor/D2.png', 2160),
+        new BackgroundObjects('img/3. Background/Layers/1. Light/2.png', 2160),
     ];
 
     canvas;
