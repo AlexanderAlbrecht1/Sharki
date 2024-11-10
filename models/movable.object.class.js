135060ca7e1f35e34cdcaace012fe2b0ebc7c898
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-11-10 23:44:51 +0100

    start static object in canvas

--- models/movable.object.class.js
+++ models/movable.object.class.js
@@ -1,4 +1,4 @@
-class movableObject {
+class movableObject extends DrawableObject {
     x = 120;
     y = 250;
     img;
@@ -30,22 +30,22 @@ class movableObject {
         return this.y < 0
     }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
+    // loadImage(path) {
+    //     this.img = new Image();
+    //     this.img.src = path;
+    // }
 
-    loadImages(arr) {
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+    // loadImages(arr) {
+    //     arr.forEach(path => {
+    //         let img = new Image();
+    //         img.src = path;
+    //         this.imageCache[path] = img;
+    //     });
+    // }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+    // draw(ctx) {
+    //     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    // }
 
     drawFrame(ctx) {
         if (this instanceof Character || this instanceof pufferFish || this instanceof JellyFish || this instanceof Endboss) {
